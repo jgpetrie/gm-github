c29ed1b1056483449e188b1eeb8b8fc891d2ca21
jgpetrie <jpetrie@umich.edu>
2016-05-03 15:55:11 -0400

    Merged changes

--- read_gm_data.R
+++ read_gm_data.R
@@ -1,10 +1,9 @@
 gdp <- read.csv("data/data-1952.csv")
 new <- read.csv("data/data-1957.csv")
 
-# Calculating GDP by tiffert
 gdp <- rbind(gdp,new)
 
-#JGPETRIE changed the formula
+#Calculating GDP
 gdp$gdp <- gdp$pop*gdp$gdpPercap
 
 keep <- c('country', 'year', 'lifeExp', 'gdp')
